461396d8b7ac0e6220efdabd648f837a534bc0c7
Grape <putao@ecook.cn>
2018-12-11 15:43:04 +0800

    first init

---
+++ src/main/java/com/nightstory/mineboot/MinebootApplication.java
@@ -0,0 +1,12 @@
+package com.nightstory.mineboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinebootApplication.class, args);
+	}
+}
